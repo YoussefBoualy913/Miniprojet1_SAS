be76a490bd4f6d22877d0cd87fd4e8df2247ad4c
youssef boualy <youssefboua913@gmail.com>
2025-09-04 12:31:53 +0200

    les fonction

--- projet1.c
+++ projet1.c
@@ -1,7 +1,44 @@
+
 #include <stdio.h>
 
+
+float calculerMoyenne(int notes[], int n){
+    int som=0;
+    for(int i=0 ;i<n;i++){
+        som+=notes[i];
+    }
+    return som/n;
+}
+int trouverMax(int notes[], int n){
+    int max=notes[0];
+    for(int i=0 ;i<n;i++){
+        if(notes[i]>max){
+            max=notes[i];
+        }
+    }
+    return max;
+}
+int trouverMin(int notes[], int n){
+     int min=notes[0];
+    for(int i=0 ;i<n;i++){
+        if(notes[i]<min){
+            min=notes[i];
+        }
+    }
+    return min;
+}
+int compterReussites(int notes[], int n){
+    int c=0;
+    for(int i=0;i<n;i++){
+        if(notes[i]>=10){
+            c++;
+        }
+    }
+    return c;
+}
 int main() {
-   int tailltab;
+   int  tailltab,Max,Min,Nobrederussit;
+   float moyen;
    do{
     printf("entrez le nombre des etudiant:\n");
     scanf("%d",&tailltab);}while(tailltab<0 || tailltab>30);
@@ -9,6 +46,14 @@ int main() {
     for(int i=0;i<tailltab;i++){
         printf("Saisir la not de l etudiant %d:",i+1);
         scanf("%d",&tabE[i]);}
-
-    return 0;
+        moyen=calculerMoyenne(tabE,tailltab);
+        Max=trouverMax(tabE,tailltab);
+        Min=trouverMin(tabE,tailltab);
+        Nobrederussit=compterReussites(tabE,tailltab);
+    printf("Le Moyenne:%.2f\n",moyen);
+    printf("le Maximmum:%d\n",Max);
+    printf("le Minimmum:%d\n",Min);
+    printf("le nombre de ressites:%d\n",Nobrederussit);
+return 0;
 }
+    
